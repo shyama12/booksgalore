99114f625bae1a08d47532a436dbe6722f8de6e6
ana mikic <mikicana@protonmail.com>
2022-11-04 10:59:32 +0100

    image upload added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,6 +23,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :genre, :author, :price, :summary)
+    params.require(:book).permit(:title, :genre, :author, :price, :summary, :photo)
   end
 end
