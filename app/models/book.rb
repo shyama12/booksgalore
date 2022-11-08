b14f4921ab37ffd509039c754022fb7d135ffa92
Shyama Menon <shyama12@hotmail.com>
2022-11-08 10:21:06 +0100

    Various bug fixes

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :summary, presence: true, length: { minimum: 2, maximum: 1000 }
+  validates :price, presence: true, numericality: true
+  validates :photo, presence: true
 end
