03ae8bc6ce5d230bfb475482cc69a5454af2b9d6
Seb Rojas <src.py@icloud.com>
2022-11-09 18:00:08 +0100

    limiting book of the week section to 4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @books = Book.where("price > ?", 1)
+    @books = Book.where("price > ?", 1).limit(4)
   end
 end
