74b347df3fb76cf7e82451ad90d5fdbbc75fd09f
ana mikic <mikicana@protonmail.com>
2022-11-10 20:42:59 +0100

    adding search option

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
   def index
-    @books = Book.all
+    if params[:query].present?
+      @books = Book.search_by_title_and_author_and_genre(params[:query])
+    else
+      @books = Book.all
+    end
   end
 
   def new
